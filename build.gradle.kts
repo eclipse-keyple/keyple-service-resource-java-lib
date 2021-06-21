607c46a6490e3470a78540aa8e0a2ca4f6dd4329
Andrei Cristea <andrei.cristea019@gmail.com>
2021-06-21 16:23:58 +0200

    Release 2.0.0-rc1

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,10 +30,10 @@ repositories {
     maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots")
 }
 dependencies {
-    implementation("org.calypsonet.terminal:calypsonet-terminal-reader-java-api:1.0-SNAPSHOT") { isChanging = true }
-    implementation("org.eclipse.keyple:keyple-common-java-api:2.0-SNAPSHOT") { isChanging = true }
-    implementation("org.eclipse.keyple:keyple-service-java-lib:2.0.0-SNAPSHOT") { isChanging = true }
-    implementation("org.eclipse.keyple:keyple-util-java-lib:2.0.0-SNAPSHOT") { isChanging = true }
+    implementation("org.calypsonet.terminal:calypsonet-terminal-reader-java-api:1.0-rc1")
+    implementation("org.eclipse.keyple:keyple-common-java-api:2.0-rc1")
+    implementation("org.eclipse.keyple:keyple-service-java-lib:2.0.0-rc1")
+    implementation("org.eclipse.keyple:keyple-util-java-lib:2.0.0-rc1")
     implementation("com.google.code.gson:gson:2.8.5")
     implementation("org.slf4j:slf4j-api:1.7.25")
     testImplementation("org.slf4j:slf4j-simple:1.7.25")
