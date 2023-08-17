d3cb9f9fc35a423feb920790e0255bf86e276b78
Andrei Cristea <andrei.cristea019@gmail.com>
2023-08-17 10:06:33 +0200

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,9 +30,10 @@ repositories {
     maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots")
 }
 dependencies {
-    implementation("org.eclipse.keypop:keypop-reader-java-api:2.0.0-SNAPSHOT")
+    implementation("org.eclipse.keypop:keypop-reader-java-api:2.0.0-SNAPSHOT") {isChanging=true}
     implementation("org.eclipse.keyple:keyple-common-java-api:2.0.0")
-    implementation("org.eclipse.keyple:keyple-service-java-lib:3.0.0-SNAPSHOT")
+    implementation("org.eclipse.keyple:keyple-service-java-lib:3.0.0-SNAPSHOT") {isChanging=true}
+    implementation("org.eclipse.keyple:keyple-common-java-api:2.0.0")
     implementation("org.eclipse.keyple:keyple-util-java-lib:2.3.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.slf4j:slf4j-api:1.7.32")
